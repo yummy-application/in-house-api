2e4ab3ee1f156309ee8b8c7e93e63afb08531936
HumanBot000 <tobias.sarosi@gmail.com>
2024-09-27 16:51:04 +0200

    added global backend communication

--- setup/createRestaurant.py
+++ setup/createRestaurant.py
@@ -1,10 +1,12 @@
 import json
 import flask
 from flask import Response
-
+import globalApiCommunication.restaurantCreation
 import database.auth
 import database.restaurant
 import validation.HashPassword
+
+
 create_restaurant = flask.Blueprint("create_restaurant", __name__)
 
 
@@ -14,8 +16,10 @@ def register_restaurant():
     restaurant_name = request_data.get('name')
     restaurant_image = request_data.get('image')
     password = request_data.get('password')
+    backend_address = request_data.get('backendAddress')
     if database.restaurant.restaurant_already_exists():
         return Response("There is already another restaurant registered at this ip", status=409)
     database.auth.set_password(password)
     database.restaurant.create_restaurant(restaurant_name, restaurant_image)
+    globalApiCommunication.restaurantCreation.create_restaurant(restaurant_name, restaurant_image, password,backend_address)
     return Response("Ok",200)
